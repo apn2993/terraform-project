8ec1f70fe4d3696bbab60b43363c59aa4c92158a
apn2993 <132740908+apn2993@users.noreply.github.com>
2023-07-20 11:17:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 #this file consists of code for instances and sg
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIA3MMKURQNN63FQEGU"
-secret_key = "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"
+access_key = ""
+secret_key = ""
 }
 
 resource "aws_instance" "one" {
